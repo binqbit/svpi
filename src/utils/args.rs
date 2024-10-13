01211571cdbee8cfaad695351c90e5258452b209
binqbit <128422584+binqbit@users.noreply.github.com>
2024-10-13 06:57:53 +0300

    Add argument processing module for command, flags and params handling

---
+++ src/utils/args.rs
@@ -0,0 +1,26 @@
+
+pub fn get_command() -> Option<String> {
+    std::env::args().nth(1)
+}
+
+fn skip_flags() -> usize {
+    let mut skip = 2;
+    for arg in std::env::args().skip(2) {
+        if arg.starts_with("--") || arg.starts_with("-") {
+            skip += 1;
+        } else {
+            break;
+        }
+    }
+    skip
+}
+
+pub fn get_flag(flags: Vec<&str>) -> Option<String> {
+    std::env::args()
+        .skip(2)
+        .find(|arg| flags.contains(&arg.as_str()))
+}
+
+pub fn get_param(id: usize) -> Option<String> {
+    std::env::args().nth(skip_flags() + id)
+}
