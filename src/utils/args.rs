6bd4a0f9c1019e83986d6642a1c0553faea9c0fb
binqbit <128422584+binqbit@users.noreply.github.com>
2025-09-05 00:11:06 +0300

    Refactor argument handling in utils: streamline flag checking and simplify password retrieval logic

--- src/utils/args.rs
+++ src/utils/args.rs
@@ -2,24 +2,22 @@ pub fn get_command() -> Option<String> {
     std::env::args().nth(1)
 }
 
-fn skip_flags() -> usize {
-    let mut skip = 2;
-    for arg in std::env::args().skip(2) {
-        if arg.starts_with("--") || arg.starts_with("-") {
-            skip += 1;
-        } else {
-            break;
-        }
-    }
-    skip
+pub fn get_param_by_id(id: usize) -> Option<String> {
+    std::env::args().nth(id + 2)
 }
 
-pub fn check_flag(flags: Vec<&str>) -> bool {
+pub fn get_param_by_flag(flag: &str) -> Option<String> {
+    let mut iter = std::env::args().skip(2);
+    while let Some(arg) = iter.next() {
+        if arg == flag {
+            return iter.next();
+        }
+    }
+    None
+}
+
+pub fn check_flags(flags: &[&str]) -> bool {
     std::env::args()
         .skip(2)
         .any(|arg| flags.contains(&arg.as_str()))
-}
-
-pub fn get_param(id: usize) -> Option<String> {
-    std::env::args().nth(skip_flags() + id)
 }
--- src/utils/terminal.rs
+++ src/utils/terminal.rs
@@ -2,56 +2,50 @@ use std::io::Write;
 
 use rpassword::read_password;
 
-use super::args;
-
-pub fn confirm(text: &str) -> bool {
-    print!("{} (y/N): ", text);
+pub fn confirm(msg: &str) -> bool {
+    print!("{} (y/N): ", msg);
     std::io::stdout().flush().unwrap();
+
     let mut confirm = String::new();
     std::io::stdin()
         .read_line(&mut confirm)
         .expect("Failed to read answer");
-    confirm.trim() == "y"
+
+    confirm.to_lowercase().trim() == "y"
 }
 
-pub fn get_password(check_flag: bool, confirm: bool, text: Option<String>) -> Option<String> {
-    let text = text.unwrap_or("Password".to_string());
-    let password_flag = args::check_flag(vec![
-        "--password",
-        "-p",
-        "--root-password",
-        "-rp",
-        "--password2",
-        "-p2",
-        "--root-password2",
-        "-rp2",
-    ]);
-    let is_confirm_password = confirm || args::check_flag(vec!["--confirm", "-cp"]);
+pub fn get_password(title: Option<&str>) -> Option<String> {
+    let title = title.unwrap_or("password");
 
-    if !check_flag || password_flag {
-        loop {
-            print!("{text}: ");
-            std::io::stdout().flush().unwrap();
-            let password = read_password().unwrap().trim().to_string();
-            if password.is_empty() {
-                return None;
-            }
+    loop {
+        print!("Enter {title}: ");
+        std::io::stdout().flush().unwrap();
 
-            if is_confirm_password {
-                print!("Confirm {text}: ");
-                std::io::stdout().flush().unwrap();
-                let confirm_password = read_password().unwrap().trim().to_string();
-                if !confirm_password.is_empty() {
-                    if password != confirm_password {
-                        println!("Passwords do not match!");
-                        println!("Please try again.");
-                        continue;
-                    }
-                }
-            }
-            return Some(password);
+        let password = read_password()
+            .expect("Failed to read password")
+            .trim()
+            .to_string();
+
+        if password.is_empty() {
+            return None;
         }
-    } else {
-        return None;
+
+        print!("Confirm {title}: ");
+        std::io::stdout().flush().unwrap();
+
+        let confirm_password = read_password()
+            .expect("Failed to read password")
+            .trim()
+            .to_string();
+
+        if !confirm_password.is_empty() {
+            if password != confirm_password {
+                println!("Passwords do not match!");
+                println!("Please try again.");
+                continue;
+            }
+        }
+
+        return Some(password);
     }
 }
