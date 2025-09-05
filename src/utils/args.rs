ede5b741068dd1a13a8c63416d0ad612f24102a3
binqbit <128422584+binqbit@users.noreply.github.com>
2025-09-05 12:05:44 +0300

    fix: update version to 4.1.1 and improve argument parsing logic in args.rs

--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,16 +1,32 @@
 pub fn get_command() -> Option<String> {
-    std::env::args().nth(1)
+    for arg in std::env::args().skip(1) {
+        if !arg.starts_with('-') {
+            return Some(arg);
+        }
+    }
+    None
 }
 
 pub fn get_param_by_id(id: usize) -> Option<String> {
-    std::env::args().nth(id + 2)
+    let mut id = id + 1;
+    for arg in std::env::args().skip(1) {
+        if !arg.starts_with('-') {
+            if id == 0 {
+                return Some(arg);
+            }
+            id -= 1;
+        }
+    }
+    None
 }
 
 pub fn get_param_by_flag(flag: &str) -> Option<String> {
-    let mut iter = std::env::args().skip(2);
+    let flag = format!("{}=", flag);
+    let mut iter = std::env::args().skip(1);
     while let Some(arg) = iter.next() {
-        if arg == flag {
-            return iter.next();
+        if arg.starts_with(&flag) {
+            let value = arg[flag.len()..].to_string();
+            return Some(value);
         }
     }
     None
@@ -18,6 +34,6 @@ pub fn get_param_by_flag(flag: &str) -> Option<String> {
 
 pub fn check_flags(flags: &[&str]) -> bool {
     std::env::args()
-        .skip(2)
-        .any(|arg| flags.contains(&arg.as_str()))
+        .skip(1)
+        .any(|arg| flags.iter().any(|flag| arg.starts_with(flag)))
 }
