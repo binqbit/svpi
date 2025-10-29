4faf3edfd0f577fd310a8ceb36cc92b8ca4ee4cc
Dmitry <binqbit@gmail.com>
2025-10-29 03:01:54 +0200

    fix: refactor add_encryption_key and link_key methods to use EncryptionKey struct and improve fingerprint handling

--- src/pass_mgr/password.rs
+++ src/pass_mgr/password.rs
@@ -1,9 +1,7 @@
 use crate::{
     pass_mgr::{PasswordManager, PasswordManagerError},
-    seg_mgr::{Data, DataType, MASTER_PASSWORD_HASH_SIZE},
-    utils::crypto::{
-        derive_encryption_key, get_master_password_check, password_hash, DEFAULT_PASSWORD_HASH_SALT,
-    },
+    seg_mgr::{DataType, EncryptionKey, EncryptionLevel, SegmentError, MASTER_PASSWORD_HASH_SIZE},
+    utils::crypto::get_master_password_check,
 };
 
 impl PasswordManager {
@@ -36,22 +34,104 @@ impl PasswordManager {
         master_password: &str,
         name: &str,
         password: &str,
+        level: EncryptionLevel,
     ) -> Result<bool, PasswordManagerError> {
-        let encryption_key = derive_encryption_key(master_password.as_bytes(), name.as_bytes());
-        let password_fingerprint = password_hash(password.as_bytes(), DEFAULT_PASSWORD_HASH_SALT);
-        let data = Data::Binary(encryption_key.to_vec())
-            .encrypt(password.as_bytes())
+        let mut encryption_key = EncryptionKey::new(master_password, name, level);
+        encryption_key
+            .encrypt(password)
             .map_err(PasswordManagerError::EncryptionError)?;
+        let data = encryption_key.pack();
+
+        let password_hash = encryption_key.get_password_fingerprint(password);
 
         self.0
-            .set_segment(
-                name,
-                &data,
-                DataType::EncryptionKey,
-                Some(password_fingerprint),
-            )
+            .set_segment(name, &data, DataType::EncryptionKey, Some(password_hash))
             .map(|seg| seg.is_some())
             .map_err(PasswordManagerError::AddEncryptionKey)
+    }
+
+    pub fn link_key(&mut self, name: &str, password: &str) -> Result<(), PasswordManagerError> {
+        let (fingerprint, key) = self.get_encryption_key(password, None)?;
+
+        let segment = if let Some(segment) = self.0.find_segment_by_name(name) {
+            segment
+        } else {
+            return Err(PasswordManagerError::ReadPasswordError(
+                SegmentError::NotFound(name.to_string()),
+            ));
+        };
+
+        let data_type = segment.info.data_type;
+        let data = segment
+            .read_data()
+            .map_err(PasswordManagerError::ReadPasswordError)?
+            .to_bytes()
+            .map_err(|err| PasswordManagerError::ReadPasswordError(SegmentError::DataError(err)))?;
+        let _ = data_type
+            .decrypt(&data, &key)
+            .map_err(PasswordManagerError::InvalidEncryptionKey)?;
+
+        segment.info.password_fingerprint = Some(fingerprint);
+        segment
+            .update_meta()
+            .map_err(PasswordManagerError::SavePasswordError)?;
+
+        Ok(())
+    }
+
+    pub fn sync_encryption_keys(
+        &mut self,
+        master_password: &str,
+    ) -> Result<(), PasswordManagerError> {
+        let mut encryption_keys = vec![];
+        for key in self.get_encryption_keys() {
+            let encryption_key = EncryptionKey::unpack(
+                &key.read_data()
+                    .map_err(PasswordManagerError::ReadPasswordError)?
+                    .to_bytes()
+                    .map_err(|err| {
+                        PasswordManagerError::ReadPasswordError(SegmentError::DataError(err))
+                    })?,
+            )
+            .map_err(PasswordManagerError::InvalidEncryptionKey)?;
+
+            let encryption_key = EncryptionKey::new(
+                master_password,
+                key.get_name().as_str(),
+                encryption_key.level,
+            );
+
+            encryption_keys.push((encryption_key, key.info.fingerprint.fingerprint));
+        }
+
+        let segments = self
+            .0
+            .get_active_segments_mut()
+            .into_iter()
+            .filter(|seg| seg.info.data_type != DataType::EncryptionKey)
+            .collect::<Vec<_>>();
+
+        for segment in segments {
+            let data_type = segment.info.data_type;
+            let data = segment
+                .read_data()
+                .map_err(PasswordManagerError::ReadPasswordError)?
+                .to_bytes()
+                .map_err(|err| {
+                    PasswordManagerError::ReadPasswordError(SegmentError::DataError(err))
+                })?;
+
+            for (encryption_key, fingerprint) in &encryption_keys {
+                if data_type.decrypt(&data, &encryption_key.key).is_ok() {
+                    segment.info.password_fingerprint = Some(*fingerprint);
+                    segment
+                        .update_meta()
+                        .map_err(PasswordManagerError::SavePasswordError)?;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -85,10 +165,92 @@ mod tests {
         let mut mgr = setup_mgr();
         mgr.set_master_password("master").unwrap();
         assert!(mgr
-            .add_encryption_key("master", "key1", "pwd")
+            .add_encryption_key("master", "key1", "pwd", EncryptionLevel::default())
             .expect("add"));
         let seg = mgr.0.find_segment_by_name("key1").unwrap();
         assert_eq!(seg.info.data_type, DataType::EncryptionKey);
         assert!(seg.info.password_fingerprint.is_some());
+    }
+
+    #[test]
+    fn link_key_hash_to_data() {
+        let mut mgr = setup_mgr();
+        mgr.set_master_password("master").unwrap();
+        mgr.add_encryption_key("master", "key1", "pwd", EncryptionLevel::default())
+            .unwrap();
+        let old_key_fp = mgr
+            .0
+            .find_segment_by_name("key1")
+            .unwrap()
+            .info
+            .fingerprint
+            .fingerprint;
+
+        mgr.save_password("test", "data", Some("pwd".to_string()))
+            .unwrap();
+
+        mgr.remove_password("key1").unwrap();
+        mgr.add_encryption_key("master", "key1", "pwd", EncryptionLevel::default())
+            .unwrap();
+        let new_key_fp = mgr
+            .0
+            .find_segment_by_name("key1")
+            .unwrap()
+            .info
+            .fingerprint
+            .fingerprint;
+
+        mgr.link_key("test", "pwd").unwrap();
+        let new_data_fp = mgr
+            .0
+            .find_segment_by_name("test")
+            .unwrap()
+            .info
+            .password_fingerprint
+            .unwrap();
+
+        assert_ne!(old_key_fp, new_key_fp);
+        assert_eq!(new_data_fp, new_key_fp);
+    }
+
+    #[test]
+    fn sync_keys_hash_to_data() {
+        let mut mgr = setup_mgr();
+        mgr.set_master_password("master").unwrap();
+        mgr.add_encryption_key("master", "key1", "pwd", EncryptionLevel::default())
+            .unwrap();
+        let old_key_fp = mgr
+            .0
+            .find_segment_by_name("key1")
+            .unwrap()
+            .info
+            .fingerprint
+            .fingerprint;
+
+        mgr.save_password("test", "data", Some("pwd".to_string()))
+            .unwrap();
+
+        mgr.remove_password("key1").unwrap();
+        mgr.add_encryption_key("master", "key1", "pwd", EncryptionLevel::default())
+            .unwrap();
+        let new_key_fp = mgr
+            .0
+            .find_segment_by_name("key1")
+            .unwrap()
+            .info
+            .fingerprint
+            .fingerprint;
+
+        mgr.sync_encryption_keys("master").unwrap();
+        let new_data_fp = mgr
+            .0
+            .find_segment_by_name("test")
+            .unwrap()
+            .info
+            .password_fingerprint
+            .unwrap();
+
+        assert_ne!(old_key_fp, new_key_fp);
+        assert_eq!(new_data_fp, new_key_fp);
     }
 }
