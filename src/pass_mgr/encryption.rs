45ece87a05b3cdb78de7a49d8da24a460678bb25
Dmitry <binqbit@gmail.com>
2025-10-29 03:01:37 +0200

    fix: refactor get_encryption_key to improve fingerprint handling and decryption logic

--- src/pass_mgr/encryption.rs
+++ src/pass_mgr/encryption.rs
@@ -1,7 +1,6 @@
 use crate::{
     pass_mgr::{PasswordManager, PasswordManagerError},
-    seg_mgr::{DataType, Segment, DATA_FINGERPRINT_SIZE},
-    utils::crypto::{password_hash, DEFAULT_PASSWORD_HASH_SALT},
+    seg_mgr::{DataType, EncryptionKey, Segment, DATA_FINGERPRINT_SIZE},
 };
 
 impl PasswordManager {
@@ -15,35 +14,44 @@ impl PasswordManager {
 
     pub fn get_encryption_key(
         &mut self,
-        key: &str,
+        password: &str,
+        fingerprint: Option<[u8; DATA_FINGERPRINT_SIZE]>,
     ) -> Result<([u8; DATA_FINGERPRINT_SIZE], Vec<u8>), PasswordManagerError> {
-        let fingerprint = password_hash(key.as_bytes(), DEFAULT_PASSWORD_HASH_SALT);
+        if let Some(fp) = fingerprint {
+            let encryption_key_segment = self
+                .get_encryption_keys()
+                .into_iter()
+                .find(|seg| seg.info.fingerprint.fingerprint == fp);
 
-        let encryption_key_segment = self
-            .get_encryption_keys()
-            .into_iter()
-            .find(|seg| seg.info.password_fingerprint == Some(fingerprint));
+            if let Some(encryption_key_segment) = encryption_key_segment {
+                let data = encryption_key_segment
+                    .read_data()
+                    .map_err(PasswordManagerError::GetEncryptionKey)?
+                    .to_bytes()
+                    .map_err(PasswordManagerError::InvalidEncryptionKey)?;
+                let mut key = EncryptionKey::unpack(&data)
+                    .map_err(PasswordManagerError::InvalidEncryptionKey)?;
+                key.decrypt(password)
+                    .map_err(PasswordManagerError::InvalidEncryptionKey)?;
+                return Ok((encryption_key_segment.info.fingerprint.fingerprint, key.key));
+            }
+        }
 
-        if let Some(encryption_key_segment) = encryption_key_segment {
-            let data = encryption_key_segment
+        for seg in self.get_encryption_keys() {
+            let data = seg
                 .read_data()
                 .map_err(PasswordManagerError::GetEncryptionKey)?
                 .to_bytes()
                 .map_err(PasswordManagerError::InvalidEncryptionKey)?;
-            let data = encryption_key_segment
-                .info
-                .data_type
-                .decrypt(&data, key.as_bytes())
-                .map_err(PasswordManagerError::InvalidEncryptionKey)?
-                .to_bytes()
-                .map_err(PasswordManagerError::InvalidEncryptionKey)?;
-            Ok((
-                encryption_key_segment.info.fingerprint.fingerprint,
-                data.to_vec(),
-            ))
-        } else {
-            Ok((fingerprint, key.as_bytes().to_vec()))
+            let mut key =
+                EncryptionKey::unpack(&data).map_err(PasswordManagerError::InvalidEncryptionKey)?;
+            if key.decrypt(password).is_ok() {
+                return Ok((seg.info.fingerprint.fingerprint, key.key));
+            }
         }
+
+        let key = EncryptionKey::from(password);
+        Ok((key.get_password_fingerprint(password), key.key))
     }
 }
 
@@ -52,8 +60,7 @@ mod tests {
     use super::*;
     use crate::{
         data_mgr::DataInterfaceType,
-        seg_mgr::{Data, DataType},
-        utils::crypto::{password_hash, DEFAULT_PASSWORD_HASH_SALT},
+        seg_mgr::{DataType, EncryptionLevel},
     };
 
     fn setup_mgr() -> PasswordManager {
@@ -68,28 +75,37 @@ mod tests {
     #[test]
     fn get_encryption_key_not_found_returns_input() {
         let mut mgr = setup_mgr();
-        let (fp, key) = mgr.get_encryption_key("pwd").unwrap();
+        let (fp, key) = mgr.get_encryption_key("pwd", None).unwrap();
         assert_eq!(key, b"pwd");
-        assert_eq!(fp, password_hash(b"pwd", DEFAULT_PASSWORD_HASH_SALT));
+        assert_eq!(
+            fp,
+            EncryptionKey::from("pwd").get_password_fingerprint("pwd")
+        );
     }
 
     #[test]
     fn get_encryption_key_finds_stored_key() {
         let mut mgr = setup_mgr();
-        let enc_key = b"secretkey";
         let password = "pwd";
-        let password_fp = password_hash(password.as_bytes(), DEFAULT_PASSWORD_HASH_SALT);
-        let data = Data::Binary(enc_key.to_vec())
-            .encrypt(password.as_bytes())
-            .unwrap();
+
+        let mut key = EncryptionKey::new("secretkey", "test", EncryptionLevel::default());
+        let expected_plain_key = key.key.clone();
+        key.encrypt(password).expect("encrypt");
+        let data = key.pack();
+
         mgr.0
-            .set_segment("enc", &data, DataType::EncryptionKey, Some(password_fp))
-            .unwrap();
+            .set_segment(
+                "enc",
+                &data,
+                DataType::EncryptionKey,
+                Some(key.get_password_fingerprint("pwd")),
+            )
+            .expect("set segment");
 
         let expected_fp = crate::seg_mgr::DataFingerprint::get_fingerprint(&data);
-        let (fp, key) = mgr.get_encryption_key(password).unwrap();
+        let (fp, key) = mgr.get_encryption_key("pwd", None).expect("get key");
         assert_eq!(fp, expected_fp);
-        assert_eq!(key, enc_key);
+        assert_eq!(key, expected_plain_key);
 
         let keys = mgr.get_encryption_keys();
         assert_eq!(keys.len(), 1);
